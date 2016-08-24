cf6353b0e2f05d9cda75e6aaab71dae73eff7724
nachomacaso <nathanmacaso@gmail.com>
2016-08-24 18:37:28 -0500

    Modular exercises

--- intern.rb
+++ intern.rb
@@ -1,67 +1,41 @@
+module Emailing
+  def send_report
+    puts "Sending email..."
+    # use email sending library...
+    puts "Email sent!"
+  end
+end
+
 class Employee
   attr_reader :first_name, :last_name
-  # attr_writer :active
-  @@id = 0
 
   def initialize(input_options)
-    @@id += 1
-    @id = @@id
     @first_name = input_options[:first_name]
     @last_name = input_options[:last_name]
     @salary = input_options[:salary]
     @active = input_options[:active]
   end
 
-  def fire
-    if @active == true
-      @active = false
-    end
-  end
-
   def print_info
-    puts "#{@first_name} #{@last_name} makes #{amount_per_year}"
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
   end
 
   def give_annual_raise
     @salary = 1.05 * @salary
   end
-
-private 
-  def amount_per_year
-    "#{@salary} a year."
-  end
 end
 
 class Manager < Employee
+  incude Emailing
+
   def initialize(input_options)
     super(input_options)
     @employees = input_options[:employees]
   end
-
-  def send_report
-    puts "Sending email..."
-    # use email sending library...
-    puts "Email sent!"
-  end
-
-  def give_all_raises
-    @employees.each do |employee|
-      employee.give_annual_raise
-    end
-  end
-
-  def fire_all_employees
-    @employees.each do |employee|
-      # employee.active = false ( if you were to use attr_writer)
-      employee.fire
-    end
-  end
 end
 
 class Intern < Employee
-
-  def 
-
+  include Emailing
 end
 
 employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
@@ -71,10 +45,7 @@ employee2.print_info
 
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 manager.print_info
-manager.give_all_raises
+manager.send_report
 
-employee1.print_info
-employee2.print_info
-
-manager.fire_all_employees
-p employee1
+intern = Intern.new
+intern.send_report
